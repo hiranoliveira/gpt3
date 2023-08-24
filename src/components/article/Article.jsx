660e2163b79169d341ca8071044ab4e564899506
Hiran Oliveira <56979789+hiranoliveira@users.noreply.github.com>
2023-08-23 23:11:16 -0300

    <add> Project finished

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './article.css'
+
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  )
+}
+
+
+export default Article
